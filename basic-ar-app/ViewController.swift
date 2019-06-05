46e72b2cf0cc6253e625167c02f6aed3df9eb890
marcovazquez <marcovazquez77@hotmail.com>
2019-06-04 22:07:01 -0500

    Initial commit

---
+++ basic-ar-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  basic-ar-app
+//
+//  Created by Marco Vazquez on 6/4/19.
+//  Copyright © 2019 Marco Vazquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
