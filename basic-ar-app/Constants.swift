e85541b4283351f9b30cacd1686f355c083e6356
marcovazquez <marcovazquez77@hotmail.com>
2019-06-09 00:45:43 -0500

    Updated: Model and static list of models added

---
+++ basic-ar-app/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  basic-ar-app
+//
+//  Created by Marco Vazquez on 6/7/19.
+//  Copyright © 2019 Marco Vazquez. All rights reserved.
+//
+
+import Foundation
+
+enum Constants {
+    static let modelList = [Node](arrayLiteral:
+        Node(location: "1.dae", name: "1", icon: "1.png"),
+        Node(location: "2.dae", name: "2", icon: "2.png"),
+        Node(location: "3.dae", name: "3", icon: "3.png"),
+        Node(location: "4.dae", name: "4", icon: "4.png")
+    )
+}
---
+++ basic-ar-app/Node.swift
@@ -0,0 +1,15 @@
+//
+//  Node.swift
+//  basic-ar-app
+//
+//  Created by Marco Vazquez on 6/7/19.
+//  Copyright © 2019 Marco Vazquez. All rights reserved.
+//
+
+import Foundation
+
+struct Node {
+    var location: String
+    var name: String
+    var icon: String
+}
